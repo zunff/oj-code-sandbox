569ee2aec71a8fe06bf9d8657c3ba1fd3464fcf1
zhuai <1430119384@qq.com>
2024-01-22 22:14:55 +0800

    配合Api开放平台

--- src/main/java/com/zjh/ojcodesandbox/controller/ExecJavaController.java
+++ src/main/java/com/zjh/ojcodesandbox/controller/ExecJavaController.java
@@ -7,17 +7,14 @@ import com.zjh.ojcodesandbox.model.ExecuteCodeRequest;
 import com.zjh.ojcodesandbox.model.ExecuteCodeResponse;
 import com.zjh.ojcodesandbox.model.enums.ExecuteCodeStatusEum;
 import com.zjh.ojcodesandbox.util.ErrorUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/java")
+@RequestMapping("/open/exec/java")
 public class ExecJavaController {
     @Resource
     private JavaNativeCodeArgsSandbox javaNativeCodeArgsSandbox;
@@ -29,15 +26,21 @@ public class ExecJavaController {
     private JavaDockerCodeArgsSandbox javaDockerCodeArgsSandbox;
 
     //定义鉴权请求头和密钥
-    private static final String AUTH_REQUEST_HEADER = "auth";
-    private static final String AUTH_REQUEST_SECRET = "secretKey";
+//    private static final String AUTH_REQUEST_HEADER = "auth";
+//    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
+//    @GetMapping("/test")
+//    public String test(HttpServletRequest request) {
+//        String header = request.getHeader("X-Color-Tag");
+//        return "123";
+//    }
 
     @PostMapping("/native/args")
     public ExecuteCodeResponse executeCodeNativeArgs(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request, HttpServletResponse response) {
-        if (!AUTH_REQUEST_SECRET.equals(request.getHeader(AUTH_REQUEST_HEADER))) {
-            response.setStatus(403);
-            return ErrorUtils.getResponse(ExecuteCodeStatusEum.NO_AUTH);
-        }
+//        if (!AUTH_REQUEST_SECRET.equals(request.getHeader(AUTH_REQUEST_HEADER))) {
+//            response.setStatus(403);
+//            return ErrorUtils.getResponse(ExecuteCodeStatusEum.NO_AUTH);
+//        }
         if (executeCodeRequest == null) {
             return ErrorUtils.get500Response("请求参数为空");
         }
@@ -46,10 +49,14 @@ public class ExecJavaController {
 
     @PostMapping("/native/acm")
     public ExecuteCodeResponse executeCodeNativeAcm(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request, HttpServletResponse response) {
-        if (!AUTH_REQUEST_SECRET.equals(request.getHeader(AUTH_REQUEST_HEADER))) {
-            response.setStatus(403);
-            return ErrorUtils.getResponse(ExecuteCodeStatusEum.NO_AUTH);
-        }
+//        if (!AUTH_REQUEST_SECRET.equals(request.getHeader(AUTH_REQUEST_HEADER))) {
+//            response.setStatus(403);
+//            return ErrorUtils.getResponse(ExecuteCodeStatusEum.NO_AUTH);
+//        }
+//        String tag = request.getHeader("X-Color-Tag");
+//        String accessKey = request.getHeader("accessKey");
+//        String sign = request.getHeader("sign");
+
         if (executeCodeRequest == null) {
             return ErrorUtils.get500Response("请求参数为空");
         }
@@ -58,10 +65,10 @@ public class ExecJavaController {
 
     @PostMapping("/docker/args")
     public ExecuteCodeResponse executeCodeDockerArgs(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request, HttpServletResponse response) {
-        if (!AUTH_REQUEST_SECRET.equals(request.getHeader(AUTH_REQUEST_HEADER))) {
-            response.setStatus(403);
-            return ErrorUtils.getResponse(ExecuteCodeStatusEum.NO_AUTH);
-        }
+//        if (!AUTH_REQUEST_SECRET.equals(request.getHeader(AUTH_REQUEST_HEADER))) {
+//            response.setStatus(403);
+//            return ErrorUtils.getResponse(ExecuteCodeStatusEum.NO_AUTH);
+//        }
         if (executeCodeRequest == null) {
             return ErrorUtils.get500Response("请求参数为空");
         }
